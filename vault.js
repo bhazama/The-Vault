f285abaf1b4a1f0e7d10be7dce132197f1bec9b2
Brendan Hazama <brendanhazama@Brendans-MacBook-Pro.local>
2017-09-13 15:39:00 -1000

    all tests passed

--- vault.js
+++ vault.js
@@ -1,4 +1,24 @@
 'use strict';
 module.exports = function() {
-  
+  var vault = {};
+
+  return{
+    setValue : setValue,
+    getValue : getValue,
+  };
+
+
+
+
+  function setValue(key, value){
+    vault[key] = value;
+  }
+
+  function getValue(key){
+    if (key === undefined || !vault.hasOwnProperty(key)){
+      return null;
+    }else if(vault.hasOwnProperty(key)){
+      return vault[key];
+    }
+  }
 };
